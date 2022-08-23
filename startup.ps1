a8b4082b6404edc4798745c0865a15b62fb540e8
Michael Wagner <michael-wagner@micwag.de>
2022-08-23 20:00:45 +0200

    Added windows defender signature update configuration

--- startup.ps1
+++ startup.ps1
@@ -1,3 +1,9 @@
 Write-Output "$(Get-Date) Running Startup script"
 Write-Output "$(Get-Date) Setting keyboard language to de-DE"
 Set-WinUserLanguageList -LanguageList de-DE -Force
+
+Write-Output "$(Get-Date) Harden Windows Defender configuration"
+Write-Output "$(Get-Date) Set Microsoft Defender Signature update interval to 1h"
+Set-MpPreference -SignatureUpdateInterval 1
+Write-Output "$(Get-Date) Check Microsoft Defender Signature updates before every scan"
+Set-MpPreference -CheckForSignaturesBeforeRunningScan 1
